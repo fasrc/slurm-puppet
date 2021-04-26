02c54679316ec1a58d1f43e3e1be520540c536dd
Paul Edmon <pedmon@cfa.harvard.edu>
2021-04-26 15:15:41 -0400

    Need to define these variables.

--- manifests/common.pp
+++ manifests/common.pp
@@ -1,8 +1,8 @@
 # slurm::common includes all the basics for slurm that everything will need
 class slurm::common (
-  $slurm_pkgs    = ['slurm','slurm-devel','slurm-contribs','slurm-libpmi']
-  $slurm_version = 'present'
-  $pmix_version  = 'present'
+  Hash   $slurm_pkgs    = ['slurm','slurm-devel','slurm-contribs','slurm-libpmi']
+  String $slurm_version = 'present'
+  String $pmix_version  = 'present'
 ){
   ensure_packages($slurm_pkgs, {'ensure' => $slurm_version})
   ensure_packages(['pmix'], {'ensure' => $pmix_version})
