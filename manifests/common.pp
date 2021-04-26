c0486bc0b5a719aee0afa301a2f0bfc842b262fc
Paul Edmon <pedmon@cfa.harvard.edu>
2021-04-26 15:09:14 -0400

    Need an actual state for these.

--- manifests/common.pp
+++ manifests/common.pp
@@ -1,8 +1,8 @@
 # slurm::common includes all the basics for slurm that everything will need
 class slurm::common (
   $slurm_pkgs    = ['slurm','slurm-devel','slurm-contribs','slurm-libpmi']
-  $slurm_version = ''
-  $pmix_version  = ''
+  $slurm_version = 'present'
+  $pmix_version  = 'present'
 ){
   ensure_packages($slurm_pkgs, {'ensure' => $slurm_version})
   ensure_packages(['pmix'], {'ensure' => $pmix_version})
