154a7e343f35ba1e2ab676155d1cff2b56b5a09f
Paul Edmon <pedmon@cfa.harvard.edu>
2021-04-27 10:01:21 -0400

    Laying down config files.

--- manifests/master.pp
+++ manifests/master.pp
@@ -1,5 +1,66 @@
 # slurm::master contains everything a slurm master will require
-class slurm::master {
+class slurm::master (
+  String slurm_conf    => '',
+  String topology_conf => '',
+  String slurmdbd_conf => '',
+  String jobsubmit_lua => '',
+){
   include slurm::common
   include slurm::repo
+
+  file { '/slurm':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { '/slurm/archive':
+    ensure => directory,
+    owner  => 'slurm'
+    group  => 'slurm_users'
+  }
+
+  file { '/slurm/etc':
+    ensure => directory,
+    owner  => 'slurm'
+    group  => 'slurm_users'
+  }
+
+  file { '/slurm/etc/slurm':
+    ensure => directory,
+    owner  => 'slurm'
+    group  => 'slurm_users'
+  }
+
+  file { '/slurm/spool':
+    ensure => directory,
+    owner  => 'slurm'
+    group  => 'slurm_users'
+  }
+
+  file { '/slurm/etc/slurm/slurm.conf':
+    source => $slurm_conf
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { '/slurm/etc/slurm/topology.conf':
+    source => $topology_conf
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  file { '/etc/slurm/job_submit.lua':
+    source => $jobsubmit_lua
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  file { '/etc/slurm/slurmdbd.conf':
+    source => template($slurmdbd_conf),
+    owner  => 'slurm'
+    group  => 'slurm_users'
+    mode   => '0600',
+  }
 }
