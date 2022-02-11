52a5f5567bc1f7a214cbc357578e3bdf605c6846
Paul Edmon <pedmon@cfa.harvard.edu>
2022-02-11 15:38:34 -0500

    Clarifying check.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -8,7 +8,7 @@ class slurm::repo (
   String  $baseurl      = "http://mirror-proxy.rc.fas.harvard.edu/slurm-test/centos${::operatingsystemmajrelease}",
 ){
   if $manage_repo {
-    if $::osfamily =~ /(RedHat)/ {
+    if $::osfamily == 'RedHat' {
       yumrepo { 'slurm':
         ensure   => $ensure,
         descr    => 'slurm rpms',
