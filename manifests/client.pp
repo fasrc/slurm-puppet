6857259b333b91d7d7b309365e39091b2bbb71fe
Paul Edmon <pedmon@cfa.harvard.edu>
2022-07-29 10:50:43 -0400

    Adding client packages.

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,4 +1,10 @@
 # slurm::client includes everything needed for client hosts
-class slurm::client {
+class slurm::client (
+  Array   $slurm_client_pkgs   = ['slurm-slurmd','slurm-pam_slurm'],
+){
   include slurm::common
+
+  $slurm_version = $slurm::common::slurm_version
+
+  ensure_packages($slurm_client_pkgs, {'ensure' => $slurm_version})
 }
