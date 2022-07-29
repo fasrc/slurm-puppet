097556cbef566412694c0ca26a1037ec2bf01d1a
Paul Edmon <pedmon@cfa.harvard.edu>
2022-07-29 11:05:15 -0400

    Adding prolog and epilog.

--- manifests/client.pp
+++ manifests/client.pp
@@ -7,4 +7,18 @@ class slurm::client (
   $slurm_version = $slurm::common::slurm_version
 
   ensure_packages($slurm_client_pkgs, {'ensure' => $slurm_version})
+
+  file { '/usr/local/bin/slurm_task_prolog':
+    content => template('slurm/slurm_task_prolog.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  file { '/usr/local/bin/slurm_epilog':
+    content => template('slurm/slurm_epilog.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
 }
