c72a62088e20957587f6c20ce66fe251f4934fb8
Paul Edmon <pedmon@cfa.harvard.edu>
2023-03-20 13:40:08 -0400

    Adding profile.d scripts.

--- manifests/rcsqueue.pp
+++ manifests/rcsqueue.pp
@@ -1,8 +1,26 @@
 # slurm::rcsqueue installs fasrc's instance of caching squeue
 class slurm::rcsqueue (
   Boolean $enable = false,
+  String  $dbhost = '',
+  String  $dbname = '',
+  String  $dbuser = '',
+  String  $dbpass = '',
 ){
   if $enable {
     ensure_packages(['python3-mysqlclient'])
+
+    file { '/etc/profile.d/rc-squeue.sh':
+      content => template ('slurm/rc-squeue.sh.erb'),
+      owner   => root,
+      group   => root,
+      mode    => 0644,
+    }
+
+    file { '/etc/profile.d/rc-squeue.csh':
+      content => template ('slurm/rc-squeue.csh.erb'),
+      owner   => root,
+      group   => root,
+      mode    => 0644,
+    }
   }
 }
