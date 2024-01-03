e7d8c79fe5fcea34439655576960c56c843c8974
Paul Edmon <pedmon@cfa.harvard.edu>
2024-01-03 10:25:57 -0500

    Remove quotes.

--- manifests/client.pp
+++ manifests/client.pp
@@ -8,7 +8,7 @@ class slurm::client (
   String  $constrain_ram_space  = 'yes',
   String  $constrain_swap_space = 'yes',
   String  $constrain_devices    = 'yes',
-  Integer $energyipmifreq       = '30',
+  Integer $energyipmifreq       = 30,
 ){
   include slurm::common
 
