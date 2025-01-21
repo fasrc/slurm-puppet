a5527a70b30cc02f38f40bdf0e7f82cfa450c271
Paul Edmon <pedmon@cfa.harvard.edu>
2025-01-21 09:39:45 -0500

    Adding ensure.

--- manifests/rcsqueue.pp
+++ manifests/rcsqueue.pp
@@ -1,15 +1,17 @@
 # slurm::rcsqueue installs fasrc's instance of caching squeue
 class slurm::rcsqueue (
-  Boolean $enable = false,
-  String  $dbhost = '',
-  String  $dbname = '',
-  String  $dbuser = '',
-  String  $dbpass = '',
+  Boolean $enable          = false,
+  String  $ensure = 'present',
+  String  $dbhost          = '',
+  String  $dbname          = '',
+  String  $dbuser          = '',
+  String  $dbpass          = '',
 ){
   if $enable {
     ensure_packages(['python3-mysqlclient'])
 
     file { '/etc/profile.d/rc-squeue.sh':
+      ensure  => $ensure,
       content => template ('slurm/rc-squeue.sh.erb'),
       owner   => 'root',
       group   => 'root',
@@ -17,6 +19,7 @@ class slurm::rcsqueue (
     }
 
     file { '/etc/profile.d/rc-squeue.csh':
+      ensure  => $ensure,
       content => template ('slurm/rc-squeue.csh.erb'),
       owner   => 'root',
       group   => 'root',
@@ -24,6 +27,7 @@ class slurm::rcsqueue (
     }
 
     file { '/usr/local/bin/squeue':
+      ensure => $ensure,
       source => 'puppet:///modules/slurm/rc-squeue',
       owner  => 'root',
       group  => 'root',
