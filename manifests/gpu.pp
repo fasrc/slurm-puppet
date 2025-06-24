682e8393fa5aabd58e3cfddab4bf852afdbe4353
Paul Edmon <pedmon@cfa.harvard.edu>
2025-06-24 14:53:09 -0400

    Adding gpustats_help prolog and epilog scripts.

--- manifests/gpu.pp
+++ manifests/gpu.pp
@@ -5,4 +5,21 @@ class slurm::gpu {
     owner  => 'root',
     group  => 'root',
   }
+
+  file { '/etc/slurm/prolog.d/gpustats_helper.conf':
+    source  => 'puppet:///modules/slurm/prolog.d/gpustats_helper.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => File['/etc/slurm/prolog.d'],
+  }
+
+  file { '/etc/slurm/epilog.d/gpustats_helper.conf':
+    source  => 'puppet:///modules/slurm/epilog.d/gpustats_helper.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => File['/etc/slurm/epilog.d'],
+  }
+
 }
